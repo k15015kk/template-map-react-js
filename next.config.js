bff276da3a5f3fd22834d1847d066c7ff4630626
k15015kk <aitk15015kk@outlook.com>
2022-05-05 23:07:12 +0900

    サンプルで路線を表示するように実装

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    MAP_URL : process.env.MAP_URL
+  }
 }
 
 module.exports = nextConfig
---
+++ src/components/Map/index.js
@@ -0,0 +1,70 @@
+import ReactMapGL from "react-map-gl";
+import maplibregl from "maplibre-gl";
+import DeckGL from "@deck.gl/react";
+import { GeoJsonLayer } from "@deck.gl/layers";
+import { useEffect, useState } from 'react';
+
+const Map = () => {
+
+const [jsonData, setJsonData] = useState([])
+
+  useEffect(() => {
+    const data = require("../../data/N02-20_RailroadSection.json");
+    setJsonData(data.features);
+  }, []);
+
+  const [viewState, setViewState] = useState({
+    longitude: 139.7671365841117,
+    latitude: 35.68143950212949,
+    zoom: 15,
+  });
+
+  const geoJsonLayer = new GeoJsonLayer({
+    id: "geojson",
+    data: jsonData,
+    lineWidthMinPixels: 4,
+    getLineColor: (data) => {
+      if (data.properties) {
+        switch (data.properties.N02_004) {
+          case "北海道旅客鉄道":
+            return [3, 193, 61, 255];
+            break;
+          case "東日本旅客鉄道":
+            return [55, 134, 64, 255];
+            break;
+          case "東海旅客鉄道":
+            return [255, 126, 28, 255];
+            break;
+          case "西日本旅客鉄道":
+            return [0, 114, 186, 255];
+            break;
+          case "四国旅客鉄道":
+            return [0, 172, 209, 255];
+            break;
+          case "九州旅客鉄道":
+            return [246, 46, 54, 255];
+            break;
+          default:
+            return [0, 0, 0, 0];
+            break;
+        }
+      } else {
+        return [0, 0, 0, 0];
+      }
+    },
+  });
+
+  return (
+    <DeckGL
+      initialViewState={viewState}
+      layers={[geoJsonLayer]}
+      style={{ width: "100vw", height: "100vh" }}
+      controller={true}
+      onViewStateChange={(event) => setViewState(event.viewState)}
+    >
+      <ReactMapGL mapStyle={process.env.MAP_URL} mapLib={maplibregl} />
+    </DeckGL>
+  );
+};
+
+export default Map;
