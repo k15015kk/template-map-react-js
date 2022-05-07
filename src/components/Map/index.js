2ebc4cb31b45f155c500636593723d21b8c1499c
k15015kk <aitk15015kk@outlook.com>
2022-05-07 13:12:47 +0900

    新幹線を太くする変更

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -23,27 +23,42 @@ const [jsonData, setJsonData] = useState([])
   const geoJsonLayer = new GeoJsonLayer({
     id: "geojson",
     data: jsonData,
-    lineWidthMinPixels: 4,
+    lineWidthUnits: 'pixels',
+    getLineWidth: (data) => {
+      if (data.properties.N02_003) {
+        const n02_003 = data.properties.N02_003
+        const lastThreeWord = n02_003.slice(-3)
+
+        if (lastThreeWord === '新幹線') {
+          return 4
+        } else {
+          return 2
+        }
+
+      } else {
+        return 0
+      }
+    },
     getLineColor: (data) => {
       if (data.properties) {
         switch (data.properties.N02_004) {
           case "北海道旅客鉄道":
-            return [3, 193, 61, 255];
+            return [3, 193, 61, 224];
             break;
           case "東日本旅客鉄道":
-            return [55, 134, 64, 255];
+            return [55, 134, 64, 224];
             break;
           case "東海旅客鉄道":
-            return [255, 126, 28, 255];
+            return [255, 126, 28, 224];
             break;
           case "西日本旅客鉄道":
-            return [0, 114, 186, 255];
+            return [0, 114, 186, 224];
             break;
           case "四国旅客鉄道":
-            return [0, 172, 209, 255];
+            return [0, 172, 209, 224];
             break;
           case "九州旅客鉄道":
-            return [246, 46, 54, 255];
+            return [246, 46, 54, 225];
             break;
           default:
             return [0, 0, 0, 0];
